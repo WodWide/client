374fd80708fbdae9121041a1b53ed93101721a2f
Giapa <giapa@protonmail.com>
2022-01-22 16:22:38 +0200

    feat: Replace vuetify with tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from "vuetify/es5/util/colors";
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -16,11 +14,12 @@ export default {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap",
       },
+      { rel: "icon", type: "image/x-icon", href: "logo.png" },
     ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/main.scss"],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,8 +31,6 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     "@nuxt/typescript-build",
-    // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify",
     "@nuxtjs/apollo",
   ],
 
@@ -48,30 +45,6 @@ export default {
   pwa: {
     manifest: {
       lang: "en",
-    },
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ["~/assets/variables.scss"],
-    theme: {
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-        light: {
-          primary: colors.grey.darken4,
-          secondary: colors.grey.darken1,
-          accent: colors.shades.black,
-          error: colors.red.accent3,
-        },
-      },
     },
   },
   apollo: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,109 +1,14 @@
 <template>
-  <div class="index-page d-flex flex-column">
-    <v-app-bar
-      elevation="0"
-      fluid
-      light
-      class="white px-16 py-4 d-flex align-center flex-shrink-1"
-    >
-      <v-img src="/logo.png" max-height="60" max-width="110" contain />
-      <v-container flex-grow-1 class="index-page__wrapper justify-end">
-        <v-btn plain>Home</v-btn>
-        <v-btn plain>About</v-btn>
-        <v-btn plain>Pricing</v-btn>
-        <v-btn plain>Contact</v-btn>
-      </v-container>
-      <v-container class="d-flex justify-end">
-        <NuxtLink to="/login">
-          <v-btn color="primary" class="mr-2">Athlete &rarr;</v-btn>
-        </NuxtLink>
-        <NuxtLink to="/login">
-          <v-btn color="primary">Coach &rarr;</v-btn>
-        </NuxtLink>
-      </v-container>
-    </v-app-bar>
-    <v-row elevation="0" class="index-paged__row d-flex justify-center mt-16">
-      <span class="font-weight-medium display-4 mr-6">Wod</span>
-      <span class="font-weight-medium display-4 grey--text text--darken-1">
-        Wide
-      </span>
-    </v-row>
-    <v-row class="d-flex justify-center mt-16">
-      <v-img
-        src="/icon-park_rope-skipping-one.svg"
-        max-height="60"
-        max-width="110"
-        contain
-      />
-      <v-img
-        src="/healthicons_exercise.svg"
-        max-height="60"
-        max-width="110"
-        contain
-      />
-      <v-img src="/iconoir_rings.svg" max-height="60" max-width="110" contain />
-    </v-row>
-    <v-row class="d-flex justify-center mt-16">
-      A social platform for cross training athletes and coaches.
-    </v-row>
-    <v-row class="d-flex justify-center mt-16" style="z-index: 20001">
-      <v-btn color="primary" class="mr-2">Sign Up</v-btn>
-      <v-btn plain outlined>Learn More</v-btn>
-    </v-row>
-    <v-row class="mt-n16">
-      <img
-        v-for="index in 5"
-        :key="index"
-        :class="'index-page__waves--' + index"
-        :src="'/wave-' + index + '.svg'"
-      />
-    </v-row>
-  </div>
+  <div class="hello">hello</div>
 </template>
-<script>
-export default {};
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
 </script>
 <style lang="scss">
-.app-bar {
-  border: none;
-}
-.v-toolbar {
-  &__content {
-    width: 100%;
-  }
-}
-.index-page {
-  max-height: 80px;
-  &__wrapper {
-    position: relative;
-    left: 10%;
-  }
-  &__waves {
-    &--1 {
-      position: absolute;
-      bottom: 25%;
-      z-index: 1;
-    }
-    &--2 {
-      position: fixed;
-      bottom: 15%;
-      z-index: 2;
-    }
-    &--3 {
-      position: fixed;
-      bottom: 5%;
-      z-index: 3;
-    }
-    &--4 {
-      position: fixed;
-      bottom: -5%;
-      z-index: 4;
-    }
-    &--5 {
-      position: fixed;
-      bottom: -15%;
-      z-index: 5;
-    }
-  }
+.hello {
+  color: red;
 }
 </style>
