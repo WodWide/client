0693865d87df8b78eca580ec225ead5782d396b0
Giapa <giapa@protonmail.com>
2022-02-02 21:15:39 +0200

    feat: create graphql interceptor

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("stubGraphQL", (graphQlFixture) => {
+  cy.fixture(graphQlFixture).then((mockedData) => {
+    cy.on("window:before:load", (win) => {
+      function fetch({ body }: any) {
+        const { operationName } = JSON.parse(body);
+        return responseStub(mockedData[operationName]);
+      }
+      cy.stub(win, "fetch").withArgs("/graphql").as("graphql").callsFake(fetch);
+    });
+  });
+});
+
+const responseStub = (result: any) =>
+  Promise.resolve({
+    json: () => Promise.resolve(result),
+    text: () => Promise.resolve(JSON.stringify(result)),
+    ok: true,
+  });
