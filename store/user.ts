146f4ec518f9a56588937d1ddd7f3305eea0b731
Giapa <giapa@protonmail.com>
2022-02-08 20:30:18 +0200

    fix: enable typescript in stores

--- store/user.ts
+++ store/user.ts
@@ -5,27 +5,27 @@ export const state = () => ({
 });
 
 export const getters = {
-  isAuthenticated: (state) => !!state.token,
-  authStatus: (state) => state.authStatus,
-  user: (state) => state.user,
+  token: (state: { token: string }) => state.token,
+  authStatus: (state: { authStatus: Boolean }) => state.authStatus,
+  user: (state: { user: any }) => state.user,
 };
 
 export const mutations = {
-  SET_TOKEN(state, token) {
+  SET_TOKEN(state: { token: string; authStatus: boolean }, token: string) {
     state.token = token;
     state.authStatus = true;
     localStorage.setItem("apollo-token", token);
   },
-  SET_USER(state, user) {
+  SET_USER(state: { user: any }, user: any) {
     state.user = { ...user };
   },
 };
 
 export const actions = {
-  setToken({ commit }, token) {
+  setToken({ commit }: any, token: string) {
     commit("SET_TOKEN", token);
   },
-  setUser({ commit }, user) {
+  setUser({ commit }: any, user: any) {
     commit("SET_USER", user);
   },
 };
