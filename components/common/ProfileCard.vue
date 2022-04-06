47a5b688b40a5de632f4a956c0aafd3437c57092
Giapa <giapa@protonmail.com>
2022-04-06 17:12:55 +0300

    test: create test for ProfileCard component

--- components/common/ProfileCard.vue
+++ components/common/ProfileCard.vue
@@ -1,22 +1,26 @@
 <template>
   <div
-    class="flex items-center w-auto lg:w-full justify-center lg:m-8 mx-4 my-8"
+    class="profile-card flex items-center w-auto lg:w-full justify-center lg:m-8 mx-4 my-8"
   >
     <div
       class="bg-white shadow-xl rounded-lg p-2 lg:p-8 lg:w-72 flex flex-row lg:flex-col w-full"
     >
       <div class="photo-wrapper p-2">
         <img
-          class="w-32 h-32 rounded-full mx-auto"
+          class="profile-card__image w-32 h-32 rounded-full mx-auto"
           :src="image"
           alt="Profile image"
         />
       </div>
       <div class="p-2">
-        <h3 class="text-center text-xl text-gray-900 font-medium leading-8">
+        <h3
+          class="profile-card__header text-center text-xl text-gray-900 font-medium leading-8"
+        >
           <slot name="header"></slot>
         </h3>
-        <div class="text-center text-gray-400 text-sm font-semibold pt-4">
+        <div
+          class="profile-card__body text-center text-gray-400 text-sm font-semibold pt-4"
+        >
           <slot name="body"></slot>
         </div>
       </div>
