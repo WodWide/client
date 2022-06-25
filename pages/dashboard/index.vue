e3a4029197a87dca9e98cf765e38fdc53ba3fa43
yoBoyio <dimi044@hotmail.com>
2022-06-26 01:28:59 +0300

    feat: add navigation side bar

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about-page md:h-full">
+    <!-- <div
+      class="about-page__body h-full flex flex-col items-center justify-center xl:justify-start mt-4 lg:mt-12"
+    > -->
+    <SideNavbar />
+    <!-- </div> -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import SideNavbar from "@/components/layoutComponents/SideNavbar.vue";
+
+export default Vue.extend({
+  components: { SideNavbar },
+  data() {
+    return {
+      data: {
+        type: String,
+      },
+    };
+  },
+});
+</script>
+<style lang="scss"></style>
