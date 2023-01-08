0e4fbcd9ead52530f6c00ecb36821b7000c659fa
giapa <giapa@protonmail.com>
2023-01-08 16:45:56 +0200

    chore: fix naming on signup unit tests

--- pages/signup/signup.spec.ts
+++ pages/signup/signup.spec.ts
@@ -20,22 +20,22 @@ describe("Signup", () => {
     wrapper.destroy();
   });
 
-  it("renders a logo image", () => {
+  it("should render a logo image", () => {
     expect(wrapper.find("img").attributes("src")).toBe("/logo.png");
   });
 
-  it("renders a Stepper component", () => {
+  it("should render the Stepper component", () => {
     expect(wrapper.find(stubs.Stepper).exists()).toBe(true);
   });
 
-  it("displays an error message when the `error` data property is set", async () => {
+  it("should display an error message when the `error` data property is set", async () => {
     await wrapper.setData({ error: "Invalid email address" });
     expect(wrapper.find("#signup-page-error").text()).toBe(
       "Invalid email address"
     );
   });
 
-  it("calls the `signup` method when the Stepper component emits a `signup` event", () => {
+  it("should call the `signup` method when the Stepper component emits a `signup` event", () => {
     const mockMethod = jest.fn();
     wrapper.setMethods({ signup: mockMethod });
     wrapper.find(stubs.Stepper).vm.$emit("signup");
