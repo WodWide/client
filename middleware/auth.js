e0a5b4b738d0176309425ccbadadbf24a4bd9e42
giapa <giapa@protonmail.com>
2023-01-08 15:00:33 +0200

    fix: update state check on auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 export default function ({ store, redirect }) {
-  // If the user is not authenticated
-  if (!store.state.auth) {
+  if (!store.state.user.authStatus) {
     return redirect("/login");
   }
 }
